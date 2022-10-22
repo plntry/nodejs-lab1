d63e76695aa326f0512b2606b87649461248d8af
Oksana Nedilko <nedilko7b@gmail.com>
2022-10-22 15:29:12 +0300

    added prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,14 @@
 module.exports = {
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard',
-    'plugin:sonarjs/recommended'
-  ],
-  overrides: [
-  ],
+  extends: ['standard', 'plugin:sonarjs/recommended'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['sonarjs'],
-  rules: {
-  }
+  rules: {},
 }
