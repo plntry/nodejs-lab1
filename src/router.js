375ebeeda16e126767e0ebe96471325096bf6b98
Oksana Nedilko <nedilko7b@gmail.com>
2022-11-19 13:50:55 +0200

    added simple router

--- src/router.js
+++ src/router.js
@@ -1,3 +1,36 @@
+import { fileURLToPath, pathToFileURL } from 'url'
+import * as path from 'node:path'
+import { readdir } from 'node:fs/promises'
+
 const router = new Map()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const baseDir = path.join(__dirname, 'routes')
+
+async function loadRoutesDir(dirName, base) {
+  const relativePath = path.join(base, dirName)
+  const workDir = path.join(baseDir, relativePath)
+
+  const dir = await readdir(workDir, { withFileTypes: true })
+  for (const dirent of dir) {
+    if (dirent.isDirectory()) {
+      return loadRoutesDir(dirent.name, path.join(base, dirName))
+    } else if (
+      dirent.isFile() &&
+      path.extname(dirent.name) === '.js' &&
+      path.basename(dirent.name, '.js') === 'index'
+    ) {
+      const modulePath = pathToFileURL(path.join(workDir, dirent.name))
+      const module = await import(modulePath)
+      router.set(relativePath.replaceAll(path.set, '/'), { ...module })
+    }
+  }
+}
+
+await loadRoutesDir('', path.sep)
+
+console.log(router)
 
 export default router
